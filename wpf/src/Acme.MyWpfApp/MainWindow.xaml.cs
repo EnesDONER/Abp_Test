acf7f8014f9ead5b1bb67b98b64fe3064e2da7f4
Enes Doner <businessenesdoner@gmail.com>
2024-01-30 09:30:02 +0300

    Wpf App Done

---
+++ wpf/src/Acme.MyWpfApp/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows;
+
+namespace Acme.MyWpfApp;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private readonly HelloWorldService _helloWorldService;
+    private readonly SumService _sumService;
+
+    public MainWindow(HelloWorldService helloWorldService, SumService sumService)
+    {
+        _helloWorldService = helloWorldService;
+        _sumService = sumService;
+        InitializeComponent();
+    }
+
+    protected override void OnContentRendered(EventArgs e)
+    {
+        HelloLabel.Content = _helloWorldService.SayHello();
+        SumResultLabel.Content = _sumService.Sum(3,5);
+    }
+}
---
+++ wpf/src/Acme.MyWpfApp/SumService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.DependencyInjection;
+
+namespace Acme.MyWpfApp
+{
+    public class SumService: ITransientDependency
+    {
+
+        public ILogger<SumService> Logger { get; set; }
+
+        public SumService()
+        {
+            Logger = NullLogger<SumService>.Instance;
+        }
+        public int Sum(int num1,int num2)
+        {
+            int sum = num1 + num2; 
+            Logger.LogInformation(sum.ToString());
+            return sum;
+        }
+
+    }
+}
